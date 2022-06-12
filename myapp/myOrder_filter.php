f0b45acfef06ccaeda4b143d51896d6df75358e1
yayun502 <91117215+yayun502@users.noreply.github.com>
2022-06-12 12:41:36 +0800

    add files (wait for testing)

---
+++ myapp/myOrder_filter.php
@@ -0,0 +1,33 @@
+<?php
+    session_start();
+
+    $dbservername='localhost';
+    $dbname='databasehw';
+    $dbusername='root';
+    $dbpassword='';
+
+    $uacc = $_SESSION['Account']; 
+    $fsta =  $_POST['filter_status'];
+
+    if(!isset($_SESSION['Authenticated'])||$_SESSION['Authenticated']!=true){
+        header("Location: index.php");
+        exit();
+    }
+
+    $conn = new PDO("mysql:host=$dbservername;dbname=$dbname",$dbusername,$dbpassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if($fact=='All'){
+        $stmt = $conn->prepare("select * from orders where account=:account");
+        $stmt->execute(array('account'=>$uacc));
+    }
+    else{
+        $stmt = $conn->prepare("select * from orders where account=:account AND status=:status");
+        $stmt->execute(array('account'=>$uacc, 'status'=>$fsta));
+    }
+    $mrow = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    $_SESSION['filter_result'] = $mrow;
+    header("Location: myOrder.php");
+    exit();
+?>
---
+++ myapp/shopOrder_filter.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+
+    $dbservername='localhost';
+    $dbname='databasehw';
+    $dbusername='root';
+    $dbpassword='';
+
+    $sname = $_SESSION['Shop_name'];
+    $fsta =  $_POST['filter_status'];
+    
+
+    if(!isset($_SESSION['Authenticated'])||$_SESSION['Authenticated']!=true){
+        header("Location: index.php");
+        exit();
+    }
+
+    $conn = new PDO("mysql:host=$dbservername;dbname=$dbname",$dbusername,$dbpassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    if($fact=='All'){
+        $stmt = $conn->prepare("select * from orders where shop_name=:shop_name");
+        $stmt->execute(array('shop_name'=>$sname));
+    }
+    else{
+        $stmt = $conn->prepare("select * from orders where shop_name=:shop_name AND status=:status");
+        $stmt->execute(array('shop_name'=>$sname, 'status'=>$fsta));
+    }
+    $mrow = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
+    $_SESSION['filter_result'] = $mrow;
+    header("Location: shopOrder.php");
+    exit();
+?>
