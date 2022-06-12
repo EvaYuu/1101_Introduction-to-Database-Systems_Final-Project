5538e5281a07a86dba0b4b181480db2817540466
yayun502 <91117215+yayun502@users.noreply.github.com>
2022-06-12 23:27:15 +0800

    finished (但取消當下沒有及時重整頁面)

---
+++ myapp/shopOrder_cancel.php
@@ -0,0 +1,85 @@
+<?php
+    session_start();
+
+    $dbservername='localhost';
+    $dbname='databasehw';
+    $dbusername='root';
+    $dbpassword='';
+
+    $conn = new PDO("mysql:host=$dbservername;dbname=$dbname",$dbusername,$dbpassword);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    try{
+        if(!isset($_GET['OID'])){
+            throw new Exception('Something error');
+        }
+        if(!isset($_SESSION['Authenticated'])||$_SESSION['Authenticated']!=true){
+            header("Location: index.php");
+            exit();
+        }
+        $OID = $_GET['OID'];
+        
+        //get user(account) & shop(trader) info
+        $stmt = $conn->prepare("select * from orders where OID=:OID");
+        $stmt->execute(array('OID'=>$OID));
+        $row = $stmt->fetch();
+        $buyer = $row['user_account'];
+        $shop_name = $row['shop_name'];
+        $money = $row['total_price'];
+        //get user account of the shop(trader)
+        $stmt = $conn->prepare("select * from shops where shop_name=:shop_name");
+        $stmt->execute(array('shop_name'=>$shop_name));
+        $row = $stmt->fetch();
+        $seller = $row['owner']; 
+
+        $conn->beginTransaction();
+        //update order status (Not Finished->Cancel)
+        $stmt = $conn->prepare("update orders set status='Cancel' where OID=:OID");
+        $stmt->execute(array('OID'=>$OID));
+        //refund
+        //user->Receive 1.transaction 2.walletbalance
+        $stmt = $conn->prepare("insert into transactions(account, action, trader, amount_change) values(:account, 'Receive', :trader, :amount_change)");
+        $stmt->execute(array('account'=>$buyer, 'trader'=>$shop_name, 'amount_change'=>$money));
+        $stmt = $conn->prepare("update users set walletbalance=walletbalance+(:money) where account=:account");
+        $stmt->execute(array('account'=>$buyer, 'money'=>$money));
+        //shop->Payment 1.transaction 2.walletbalance
+        $money = (-1)*$money;
+        $stmt = $conn->prepare("insert into transactions(account, action, trader, amount_change) values(:account, 'Payment', :trader, :amount_change)");
+        $stmt->execute(array('account'=>$seller, 'trader'=>$buyer, 'amount_change'=>$money));
+        $stmt = $conn->prepare("update users set walletbalance=walletbalance+(:money) where account=:account");
+        $stmt->execute(array('account'=>$seller, 'money'=>$money));
+        $conn->commit();
+
+        echo <<< EOT
+            <!DOCTYPE>
+            <html>
+                <body>
+                    <script>
+                    alert('Cancel SUCCESS');
+                    window.location.replace("shopOrder.php");
+                    </script>
+                </body>
+            </html>
+        EOT;
+        exit();
+    }
+    catch(Exception $e){
+        if($conn->inTransaction()){
+            $conn->rollBack();
+            $msg='Cancel FAIL';
+        }
+        $msg = $e->getMessage();
+        echo <<< EOT
+            <!DOCTYPE>
+            <html>
+                <body>
+                    <script>
+                    alert("$msg");
+                    window.location.replace("shopOrder.php");
+                    </script>
+                </body>
+            </html>
+        EOT;
+
+    }
+?>
